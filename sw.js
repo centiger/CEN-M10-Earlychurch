ec99eaa842eff84a19908381caff0934d55e9525
centiger <centiger2@gmail.com>
2026-06-16 10:18:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,3 +3,5 @@ const ASSETS=["./", "./index.html", "./manifest.json", "./assets/earlychurch-mai
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c)).catch(()=>{});return r;}).catch(()=>caches.match(e.request)));});
+
+/* 20260616-next-era-links */
